7dbd0e562bb7732c1c058b499dacfb24ade0c957
Ryan Schachte <rschacht@asu.edu>
2015-12-16 21:35:01 -0700

    updates to model code in the views file

--- store_new_events/models.py
+++ store_new_events/models.py
@@ -7,7 +7,7 @@ class UserEvent(models.Model):
     authenticated_user = models.ForeignKey(User, unique=False, null=False, default=None)
     task_name = models.CharField(max_length=300, unique=False, default='None')
     is_google_task = models.BooleanField(default=False, unique=False)
-    google_json = models.TextField(default='NO JSON FOR THIS TASK')
+    google_json = models.TextField(default='NO JSON FOR THIS TASK', unique=True)
     start_time = models.CharField(max_length=255, default='None')
     end_time = models.CharField(max_length=255, default='None')
     url = models.CharField(max_length=255, default='None')
